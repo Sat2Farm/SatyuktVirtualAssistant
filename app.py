e6277b4a6251a8665dfc9ec93e8e41cc9ef07686
Sat2Farm <sushant.satukt@gmail.com>
2025-07-14 11:56:59 +0530

    app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ from langchain_community.vectorstores import DocArrayInMemorySearch
 from langchain_core.documents import Document
 from dotenv import load_dotenv
 import time
-
 # Load environment variables from .env file
 load_dotenv()
 
